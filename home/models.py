0312a1930c321ac7d5a28d26c1aa6abf93749e67
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-10-04 16:02:35 +0800

    Add model for cards

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+
+class ProductCard(models.Model):
+    name = models.CharField(max_length=200)
+    alt = models.CharField(max_length=50, blank=True)
+    image_url = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPage(models.Model):
+    product = models.ForeignKey(
+        ProductCard, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=50, blank=True)
+    alt = models.CharField(max_length=50, blank=True)
+    image_url = models.CharField(max_length=200)
+    active = models.BooleanField(
+        default=False, help_text="Active means this image will display first in carousel")
+
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
+from itertools import product
 from django.shortcuts import render
+from django.db.models import Q
+
+from .models import ProductCard, ProductPage
 
 
 def home(request):
-    context = {}
+    cards = ProductCard.objects.all()
+    context = {"cards": cards}
     return render(request, "base/home.html", context)
+
+
+def pages(request, pk):
+    card = ProductCard.objects.get(id=pk)
+    # pages = (product=card.id)
+    print(card)
+    context = {"pages": pages}
+    return render(request, "base/web_dev_carousel.html", context)
